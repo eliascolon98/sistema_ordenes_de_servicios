711bb86876bb6173ecff384a95d74d773aac3d41
= <eliascolon98@gmail.com>
2023-01-05 22:06:41 -0500

    documentacion con swagger parte 1

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 
 // se crea una aplicación de express
 const app = express(); 
@@ -16,5 +17,9 @@ import  servicioRoutes  from './routes/servicio.routes'; // se importan las ruta
 app.use(clienteRoutes); // se usan las rutas para la entidad "Cliente"
 app.use(tecnicoRoutes); // se usan las rutas para la entidad "Tecnico"
 app.use(servicioRoutes); // se usan las rutas para la entidad "Servicio"
+
+// Ruta para ver la documentación de los endpoints con Swagger
+const swaggerDocument: any = require('../swagger.json');
+app.use('/api/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default app; // se exporta la aplicación
--- src/entities/Tecnico.ts
+++ src/entities/Tecnico.ts
@@ -7,16 +7,16 @@ import {
 } from 'typeorm'
 
 @Entity()
-export class Tecnico extends BaseEntity{
-    @PrimaryGeneratedColumn()
-    id: number;
+export class Tecnico extends BaseEntity{ // se exporta la clase "Tecnico" que hereda de la clase "BaseEntity"
+    @PrimaryGeneratedColumn() // se marca la propiedad "id" como la columna principal y se genera su valor automáticamente
+    id: number; // se declara una propiedad "id" de tipo número
 
-    @Column()
-    nombre: string
+    @Column() // se marca la propiedad "nombre" como una columna de la tabla de base de datos
+    nombre: string // se declara una propiedad "nombre" de tipo cadena
 
-    @Column({default: true})
-    disponibilidad: boolean;
+    @Column({default: true}) // se marca la propiedad "disponibilidad" como una columna de la tabla de base de datos con un valor por defecto
+    disponibilidad: boolean; // se declara una propiedad "disponibilidad" de tipo booleano
 
-    @CreateDateColumn()
-    createat: Date;
+    @CreateDateColumn() // se marca la propiedad "createat" como una columna de tipo fecha y se genera su valor automáticamente con la fecha actual
+    createat: Date; // se declara una propiedad "createat" de tipo fecha
 }
