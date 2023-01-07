8440b5c43558bd7b62758b878e31a2032f91908f
= <eliascolon98@gmail.com>
2023-01-07 14:47:47 -0500

    ajustes de varables de entorno

--- src/database/bd.ts
+++ src/database/bd.ts
@@ -2,14 +2,14 @@ import {DataSource} from 'typeorm'
 import { Cliente } from './entities/Cliente'
 import { Tecnico } from './entities/Tecnico';
 import { Servicio } from './entities/Servicio';
-
+require('dotenv').config();
 
 let type:any = process.env.BD_DRIVE;
 let host:any = process.env.DB_HOST;
 let username:any = process.env.BD_USER;
 let password:any = process.env.BD_PASS;
 let database:any = process.env.BD_NAME;
-let port:any = process.env.PORT_PG;
+let port:any = process.env.BD_PORT;
 
 export const AppDataSource = new DataSource({
     type: type,
